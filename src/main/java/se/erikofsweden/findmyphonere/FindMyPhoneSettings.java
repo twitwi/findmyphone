e7cc4b22361b927efcb89be7ae4de5f17009be18
Rémi Emonet <remi.emonet@reverse--com.heeere>
2017-06-20 00:08:39 +0200

    If the user clears the format, it reverts to the default

--- src/main/java/se/erikofsweden/findmyphonere/FindMyPhoneSettings.java
+++ src/main/java/se/erikofsweden/findmyphonere/FindMyPhoneSettings.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 public class FindMyPhoneSettings extends PreferenceActivity {
@@ -14,6 +16,17 @@ public class FindMyPhoneSettings extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        { // Hack: if empty, set to default
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+            String k = "format_text";
+            if (pref.getString(k, "").length() == 0) {
+                pref.edit().putString(k, getString(R.string.default_format_text)).apply();
+            }
+            setPreferenceScreen(null);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
     }
     
     @Override
